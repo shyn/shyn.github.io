da260a9dd4eea7287d051895e5df4837623dcaac
dengxin <deng.x@yungku.com>
2023-11-16 10:13:50 +0800

    just use automatic-token-authentication

--- main.py
+++ main.py
@@ -279,8 +279,8 @@ def generate_rss_feed(repo, filename, me):
 
 def main(token, repo_name, issue_number=None, dir_name=BACKUP_DIR):
     user = login(token)
-    me = get_me(user)
     repo = get_repo(user, repo_name)
+    me = repo.owner.login
     # add to readme one by one, change order here
     add_md_header("README.md", repo_name)
     for func in [add_md_firends, add_md_top, add_md_recent, add_md_label, add_md_todo]:
